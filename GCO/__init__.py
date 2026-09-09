7881b7564c34b9d392fc121434bf4cf4341ded3e
Adrien <adrien.durand1709@gmail.com>
2026-09-09 14:51:52 +0200

    GCO en mode instalable

---
+++ GCO/__init__.py
@@ -0,0 +1,4 @@
+from .GCO import GCO, suboptimizer, mads_suboptimizer, trego_suboptimizer
+
+# Optionnel : exposer aussi les utilitaires directement
+from .utils import DOE, Domain, ExtendedVar
---
+++ GCO/utils/__init__.py
@@ -0,0 +1,9 @@
+"""
+# GCO/utils/__init__.py
+This module contains utility classes and functions for the GCO package.
+"""
+
+from .doe_class import DOE
+from .domain import Domain
+from .extended_var import ExtendedVar
+from .nomad_io import *
